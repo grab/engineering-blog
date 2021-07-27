eed5a45ed59e83a13334c3560d9556b765d44c5a
Travis CI <travis@travis-ci.org>
2021-07-27 03:24:50 +0000

    new version Tue Jul 27 03:24:49 UTC 2021

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const mobileMenuBtn = document.querySelector('#mobile-menu-btn');
+const mobileMenu = document.querySelector('.mobile-menu-container')
+let menuOpen = false;
+mobileMenuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    mobileMenuBtn.classList.add('open');
+    mobileMenu.classList.add('active')
+    menuOpen = true;
+  } else {
+    mobileMenuBtn.classList.remove('open');
+    mobileMenu.classList.remove('active')
+    menuOpen = false;
+  }
+});
